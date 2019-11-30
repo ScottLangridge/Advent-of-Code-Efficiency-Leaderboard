d8474f185bb46536073d341c0684afdc137a367f
Scott Langridge <scott.m.langridge@gmail.com>
2019-11-30 12:35:35 +0000

    Consts updates for JSON move and input page add

--- consts.py
+++ consts.py
@@ -1,9 +1,10 @@
 # File structure
-PLAYERS_JSON = "data/players.json"
+PLAYERS_JSON = "data/json/players.json"
 PUZZLES_JSON = "data/json/puzzles.json"
 REPO_FOLDER = 'data/player_repositories'
 
 # AOC Interface
+INPUT_PAGE_URL = 'https://adventofcode.com/%s/day/%s/input'
 ANSWER_PAGE_URL = 'https://adventofcode.com/%s/day/%s/answer'
 SESSION_COOKIE = '53616c7465645f5f13eae7e19ac2a73be79250bdabce405dae5b56366fdb789ad16c7c7ea9a9f26cece2c0f19356fd9a'
 SUCCESS_MSG = "That's the right answer!"
